7dcc9910b530770d843924cf6f73195bb23dc7c7
YourUsername <himanshusarode08@gmail.com>
2024-10-24 16:44:41 +0530

    notifications thik karado yrrrrr

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:railway_food_delivery/delivery_form.dart';
 import 'package:railway_food_delivery/firebase_options.dart';
+import 'package:railway_food_delivery/splashScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
-  runApp(
-    MyApp(),
-  );
+  runApp(MyApp());
 }
+
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   print('Handling a background message: ${message.messageId}');
   // Handle background message UI updates or actions here
@@ -36,16 +36,39 @@ class MyApp extends StatelessWidget {
           title: 'First Method',
           // You can use the library anywhere in the app even in theme
           theme: ThemeData(
+            timePickerTheme: TimePickerThemeData(
+                dayPeriodColor: Colors.orange,
+                backgroundColor: Colors.white, // Background color of the picker
+                hourMinuteTextColor:
+                    Colors.orange, // Color of the hour/minute text
+                dialHandColor: Colors.orange, // Color of the hand that moves
+                dialBackgroundColor:
+                    Colors.orange.withOpacity(0.1), // Light orange background
+                dayPeriodTextColor: MaterialStateColor.resolveWith((states) =>
+                    states.contains(MaterialState.selected)
+                        ? Colors.white
+                        : Colors.orange)!,
+                helpTextStyle:
+                    TextStyle(color: Colors.orange), // Style for the help text
+                entryModeIconColor:
+                    Colors.orange, // Color of the entry mode icon
+
+                // OK and Cancel button styles
+                cancelButtonStyle: TextButton.styleFrom(
+                  foregroundColor: Colors.orange,
+                ),
+                confirmButtonStyle:
+                    TextButton.styleFrom(foregroundColor: Colors.orange)),
             appBarTheme: AppBarTheme(
+                backgroundColor: Colors.orange,
                 centerTitle: true,
                 titleTextStyle: TextStyle(color: Colors.white, fontSize: 20.sp),
                 iconTheme: IconThemeData(color: Colors.white)),
-            primarySwatch: Colors.blue,
           ),
           home: child,
         );
       },
-      child: DeliveryForm(),
+      child: Splashscreen(),
     );
   }
 }
